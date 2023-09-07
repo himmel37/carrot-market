b9664145ac5814e86dcbaf0e612066cc2d0b8bca
himmel37 <dahamong37@gmail.com>
2023-09-07 23:33:01 +0900

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,78 @@
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      <div className="bg-blue-500">hi</div>
+    <div className="flex flex-col space-y-5 py-10">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div
+          className="border-b flex px-4 pb-5 cursor-pointer justify-between"
+          key={i}
+        >
+          <div className="flex space-x-4">
+            <div className="w-20 h-20 bg-gray-400 rounded-md" />
+            <div className="flex flex-col pt-2">
+              <h3 className="font-medium text-sm text-gray-900">
+                New iPhone 14
+              </h3>
+              <span className="text-xs text-gray-500">Black</span>
+              <span className="font-medium text-sm mt-1 text-gray-900">
+                $95
+              </span>
+            </div>
+          </div>
+          <div className="flex space-x-2 items-end justify-end">
+            <div className="flex items-center space-x-0.5 text-sm text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+            <div className="flex items-center space-x-0.5 text-sm text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>1</span>
+            </div>
+          </div>
+        </div>
+      ))}
+      <button className="fixed bg-orange-400 rounded-full text-center hover:bg-orange-500 transition-colors cursor-pointer bottom-24 right-5 shadow-xl p-4 text-white">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
